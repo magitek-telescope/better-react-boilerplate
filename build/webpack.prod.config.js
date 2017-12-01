9fe10f27eada2193fb4267caa48db69585c6dc3c
Takuma Hanatani <mail@potato4d.me>
2017-12-01 18:21:06 +0900

    Add production build config

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,8 +1,13 @@
 const baseConfig = require('./webpack.base.config')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = merge(baseConfig, {
+  output: {
+    path: path.resolve(__dirname, '../static/'),
+    filename: 'bundle.js'
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': '"production"'
